6290e8b000e67fd9676e0cc1355b3c94bf302ead
Seiksys <seikyusengsys@gmail.com>
2024-05-11 12:28:23 +0800

    new
    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,18 @@ function Footer() {
     return (
         <footer className="bg-dark text-light py-4">
             <div className="container">
-                <div className="row">
+                <div className="row"> 
                     <div className="col-md-4">
                         <h1>PcariMovie</h1>
                         <p>Lorem ipsum dolor sit amet, consec tetur adipis cing elit, sed do eiusmod tempor incididunt ut labore et.</p>
                         <h5>Join Newsletters</h5>
                         <div className="position-relative">
-                            <input type="email" className="form-control me-2" placeholder="Enter email" />
-                            <button className="btn btn-primary position-absolute top-0 start-0 translate-middle-x" style={{ left: '-30px' }}>Subscribe</button>
+                            <input type="email" className="form-control me-2 w-50" style={{ backgroundColor: 'rgba(61, 61, 61, 1)' }} placeholder="Enter email" />
+                            <button type="submit" className="btn position-absolute top-50 start-50  translate-middle-y">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" className="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                                    <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1" />
+                                </svg>
+                            </button>
                         </div>
                     </div>
                     <div className="col-md-8">
