64d6f997dd914a09ece86b213c2b1a2e3b2ce3af
Seiksys <seikyusengsys@gmail.com>
2024-05-13 17:07:03 +0800

    new
    new

--- src/App.js
+++ src/App.js
@@ -3,22 +3,86 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./components/Card.css";
 
+
 function App() {
   return (
-    <div className="">
+    <div className="bg-dark">
       <Header />
-      <div className="container-fluid bg-black">
-        <div className="container text-center mb-5 ">
+      <div className="container-fluid text-light text-center d-flex align-items-center justify-content-center" style={{ height: "666px", background: "linear-gradient(to right, rgba(0, 0, 0, 0) 10%, #000000 100%)" }}>
+        <div className="row">
+          <div className="col-6 col-md-5 px-5" >
+            <a
+              href="https://player.vimeo.com/external/488076225.hd.mp4?s=9cf4808c4e76c0a9267abb75dbec48bc451a138f&amp;profile_id=175"
+              className="glightbox_video"
+            >
+              <svg
+                width="300"
+                height="300"
+                viewBox="0 0 131 131"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="inner-circle"
+                  d="M65 21C40.1488 21 20 41.1488 20 66C20 90.8512 40.1488 111 65 111C89.8512 111 110 90.8512 110 66C110 41.1488 89.8512 21 65 21Z"
+                  fill="rgba(254, 213, 48, 1)"
+                ></path>
+                <circle
+                  className="outer_circle"
+                  cx="65.5"
+                  cy="65.5"
+                  r="64"
+                  stroke="grey"
+                  style={{
+                    strokeWidth: "1px",
+                  }}
+                ></circle>
+                <path
+                  className="play"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M60 76V57L77 66.7774L60 76Z"
+                  stroke="black"
+                  strokeWidth="2"
+                  fill="none"
+                ></path>
+                <circle
+                  className="progress"
+                  cx="65.5"
+                  cy="65.5"
+                  r="64"
+                  style={{
+                    strokeWidth: "3px",
+                    strokeDasharray: "300",
+                    strokeLinecap: "square",
+                    stroke: "rgba(254, 213, 48, 1)",
+                  }}
+                ></circle>
+              </svg>
+            </a>
+          </div>
+          <div className="col-6 col-md-6 text-start px-5">
+            <h1 className="fw-bold" style={{ fontSize: "72px", width: "600px" }}>Find your movies <br></br>here!</h1>
+            <p style={{ fontSize: "16px", width: "502px" }}>
+              Explore our gallery full of exciting films from all around the
+              globe <br></br>only your yor entertainments. No hidden charges or
+              disturbing ads.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid bg-black bg-opacity-25">
+        <div className="container text-center  ">
           <div className="row d-flex justify-content-between align-items-center mb-5 flex-wrap">
-            <div className="col-6 col-md-4 text-start fs-2 fw-bold text-light">
-              New Release
+            <div className="col-6 col-md-4 text-start fs-2 fw-bold text-light mt-5">
+              New Releases
             </div>
-            <div className="col-6 col-md-4 text-end fs-5">
+            <div className="col-6 col-md-4 text-end fs-5 mt-5">
               <button className="btn btn-link custom-btn">View More</button>
             </div>
           </div>
-          <div className="row mb-5">
-            <div className="col-md-6 col-lg-8">
+          <div className="row">
+            <div className="col-md-6 col-lg-8 mb-5">
               <a href="your_link_here">
                 <div className="card custom-card text-bg-dark">
                   <div className="fantasy">Fantasy</div>
@@ -36,7 +100,7 @@ function App() {
                         <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
                         <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
                       </svg>
-                      Duration: 2h 30min
+                      1hr 24mins
                     </div>
                     <div className="view">
                       <svg
@@ -50,7 +114,7 @@ function App() {
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                       </svg>
-                      View: 100,000
+                      21.3K views
                     </div>
                   </div>
                   <div className="titles">The Ring's Lord</div>
@@ -76,7 +140,7 @@ function App() {
                         <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
                         <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
                       </svg>
-                      Duration: 2h 30min
+                      1hr 24mins
                     </div>
                     <div className="view">
                       <svg
@@ -90,16 +154,16 @@ function App() {
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                       </svg>
-                      View: 100,000
+                      21.3K views
                     </div>
                   </div>
-                  <div className="titles">The Ring's Lord</div>
+                  <div className="titles">Interspace</div>
                 </div>
               </a>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-6 col-lg-4">
+            <div className="col-md-6 col-lg-4 mb-5">
               <a href="your_link_here">
                 <div className="card custom-card text-bg-dark">
                   <div className="fantasy">Fantasy</div>
@@ -117,7 +181,7 @@ function App() {
                         <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
                         <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
                       </svg>
-                      Duration: 2h 30min
+                      1hr 24mins
                     </div>
                     <div className="view">
                       <svg
@@ -131,10 +195,10 @@ function App() {
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                       </svg>
-                      View: 100,000
+                      21.3K views
                     </div>
                   </div>
-                  <div className="titles">The Ring's Lord</div>
+                  <div className="titles">Interspace</div>
                 </div>
               </a>
             </div>
@@ -156,7 +220,7 @@ function App() {
                         <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
                         <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
                       </svg>
-                      Duration: 2h 30min
+                      1hr 24mins
                     </div>
                     <div className="view">
                       <svg
@@ -170,10 +234,10 @@ function App() {
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                       </svg>
-                      View: 100,000
+                      21.3K views
                     </div>
                   </div>
-                  <div className="titles">The Ring's Lord</div>
+                  <div className="titles">Her & HIm</div>
                 </div>
               </a>
             </div>
@@ -195,7 +259,7 @@ function App() {
                         <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
                         <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
                       </svg>
-                      Duration: 2h 30min
+                      1hr 24mins
                     </div>
                     <div className="view">
                       <svg
@@ -209,10 +273,10 @@ function App() {
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                       </svg>
-                      View: 100,000
+                      21.3K views
                     </div>
                   </div>
-                  <div className="titles">The Ring's Lord</div>
+                  <div className="titles">Lo Lo Land</div>
                 </div>
               </a>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; // 导入 Bootstrap 样式
 
 function Header() {
   return (
-    <div className="d-flex flex-column"style={{ background: "linear-gradient(to right, rgba(0, 0, 0, 0) 1%, #000000 100%)" }}>
+    <div style={{ background: "linear-gradient(to right, rgba(0, 0, 0, 0) 10%, #000000 100%)" }}>
       <nav className="navbar navbar-expand-lg">
         <div className="container text-center">
           <a className="navbar-brand fs-2 fw-bold text-light me-4" href="#">
@@ -85,70 +85,6 @@ function Header() {
           </div>
         </div>
       </nav>
-
-      <div className="container-fluid text-light text-center d-flex align-items-center justify-content-center" style={{ height: "666px" }}>
-        <div className="row">
-          <div className="col-6 col-md-5 px-5" >
-            <a
-              href="https://player.vimeo.com/external/488076225.hd.mp4?s=9cf4808c4e76c0a9267abb75dbec48bc451a138f&amp;profile_id=175"
-              className="glightbox_video"
-            >
-              <svg
-                width="300"
-                height="300"
-                viewBox="0 0 131 131"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  className="inner-circle"
-                  d="M65 21C40.1488 21 20 41.1488 20 66C20 90.8512 40.1488 111 65 111C89.8512 111 110 90.8512 110 66C110 41.1488 89.8512 21 65 21Z"
-                  fill="rgba(254, 213, 48, 1)"
-                ></path>
-                <circle
-                  className="outer_circle"
-                  cx="65.5"
-                  cy="65.5"
-                  r="64"
-                  stroke="grey"
-                  style={{
-                    strokeWidth: "1px",
-                  }}
-                ></circle>
-                <path
-                  className="play"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M60 76V57L77 66.7774L60 76Z"
-                  stroke="black"
-                  strokeWidth="2"
-                  fill="none"
-                ></path>
-                <circle
-                  className="progress"
-                  cx="65.5"
-                  cy="65.5"
-                  r="64"
-                  style={{
-                    strokeWidth: "3px",
-                    strokeDasharray: "300",
-                    strokeLinecap: "square",
-                    stroke: "rgba(254, 213, 48, 1)",
-                  }}
-                ></circle>
-              </svg>
-            </a>
-          </div>
-          <div className="col-6 col-md-6 text-start px-5">
-            <h1 className="fw-bold" style={{ fontSize: "72px", width: "600px" }}>Find your movies <br></br>here!</h1>
-            <p style={{ fontSize: "16px", width: "502px" }}>
-              Explore our gallery full of exciting films from all around the
-              globe <br></br>only your yor entertainments. No hidden charges or
-              disturbing ads.
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
